4579657f8ab4554c4b96f39277326615d60c697c
Aleksey Logachev <a.logachev@crpo.su>
2026-01-17 23:15:17 +0300

    REFACTORING/add-shared-kernel Added a doc block

--- src/Shared/Infrastructure/Messaging/RabbitMQ/Config/RabbitMQInfrastructureConfig.php
+++ src/Shared/Infrastructure/Messaging/RabbitMQ/Config/RabbitMQInfrastructureConfig.php
@@ -6,6 +6,10 @@ namespace App\Shared\Infrastructure\Messaging\RabbitMQ\Config;
 
 final readonly class RabbitMQInfrastructureConfig
 {
+    /**
+     * @param ExchangeConfigElement[] $exchanges
+     * @param QueueConfigElement[] $queues
+     */
     public function __construct(
         public array $exchanges,
         public array $queues,
