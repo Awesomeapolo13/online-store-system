223514f29096f484eac7fbd4a1dde5642a71084a
Aleksey Logachev <a.logachev@crpo.su>
2026-03-08 13:25:59 +0300

    FEATURE/Add-cart-getting-functional Moved validation to the config file.

--- src/Order/Application/Request/GetCartRequest.php
+++ src/Order/Application/Request/GetCartRequest.php
@@ -4,23 +4,11 @@ declare(strict_types=1);
 
 namespace App\Order\Application\Request;
 
-use App\Order\Domain\Enum\RegionCodeEnum;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Validator\Constraints as Assert;
-
-final class GetCartRequest
+final readonly class GetCartRequest
 {
-    #[Assert\NotBlank]
-    #[Assert\Positive]
-    public readonly int $userId;
-
-    #[Assert\NotBlank]
-    #[Assert\Choice(callback: [RegionCodeEnum::class, 'values'])]
-    public readonly int $region;
-
-    public function __construct(Request $request)
-    {
-        $this->userId = (int) $request->query->get('user_id');
-        $this->region = (int) $request->query->get('region');
+    public function __construct(
+        public int $userId,
+        public int $region,
+    ) {
     }
 }
--- src/Order/Infrastructure/Http/Controller/GetCartController.php
+++ src/Order/Infrastructure/Http/Controller/GetCartController.php
@@ -7,38 +7,26 @@ namespace App\Order\Infrastructure\Http\Controller;
 use App\Order\Application\Request\GetCartRequest;
 use App\Order\Application\UseCase\GetCartUseCase;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\MapQueryString;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 final readonly class GetCartController
 {
     public function __construct(
-        private ValidatorInterface $validator,
         private GetCartUseCase $useCase,
     ) {
     }
 
     #[Route('/api/v1/cart', name: 'get_cart', methods: ['GET'])]
-    public function __invoke(Request $request): Response
-    {
-        $basketRequest = new GetCartRequest($request);
-
-        $violations = $this->validator->validate($basketRequest);
-        if (count($violations) > 0) {
-            $errors = [];
-            foreach ($violations as $violation) {
-                $errors[$violation->getPropertyPath()] = $violation->getMessage();
-            }
-
-            return new JsonResponse(['errors' => $errors], Response::HTTP_UNPROCESSABLE_ENTITY);
-        }
-
+    public function __invoke(
+        #[MapQueryString]
+        GetCartRequest $basketRequest,
+    ): Response {
         $result = ($this->useCase)($basketRequest);
 
         if ($result === null) {
-            return new Response(null, Response::HTTP_ACCEPTED);
+            return new JsonResponse(null, Response::HTTP_ACCEPTED);
         }
 
         return new JsonResponse($result, Response::HTTP_OK);
