3aff552b4bf88620d07f4e1ebd9191366565c9c0
Aleksey Logachev <a.logachev@crpo.su>
2026-03-21 15:03:01 +0300

    FEATURE/Add-cart-getting-functional Fixed a routing mao exception.

--- src/Shared/Infrastructure/Messaging/RabbitMQ/Routing/RoutingKeyResolver.php
+++ src/Shared/Infrastructure/Messaging/RabbitMQ/Routing/RoutingKeyResolver.php
@@ -15,23 +15,23 @@ final readonly class RoutingKeyResolver
     ) {
     }
 
-    public function resolveForCommand(CommandInterface $command): string
+    public function resolveForCommand(CommandInterface $command): ?string
     {
         $class = $command::class;
 
         if (!isset($this->commandRoutingMap[$class])) {
-            throw new \InvalidArgumentException(sprintf('No routing key configured for command "%s"', $class));
+            return null;
         }
 
         return $this->commandRoutingMap[$class];
     }
 
-    public function resolveForEvent(DomainEventInterface $event): string
+    public function resolveForEvent(DomainEventInterface $event): ?string
     {
         $class = $event::class;
 
         if (!isset($this->eventRoutingMap[$class])) {
-            throw new \InvalidArgumentException(sprintf('No routing key configured for event "%s"', $class));
+            return null;
         }
 
         return $this->eventRoutingMap[$class];
